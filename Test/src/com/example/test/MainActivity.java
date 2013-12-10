f395ddcf008cf6d4c0c4aae3387c534927ea1eb1
shinay <shinay@qq.com>
2013-12-10 21:20:05 +0800

    test3

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends Activity {
 		System.out.println("hello world!");
 		
 		System.out.println("hello world2!");
+		
+		System.out.println("hello world3!");
 	}
 
 	@Override
