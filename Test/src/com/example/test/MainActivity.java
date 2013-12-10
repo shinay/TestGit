2674fd8c2938b810ce04b92184441b1e2725e337
shinay <shinay@qq.com>
2013-12-10 21:19:35 +0800

    test2

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		System.out.println("hello world!");
+		
+		System.out.println("hello world2!");
 	}
 
 	@Override
