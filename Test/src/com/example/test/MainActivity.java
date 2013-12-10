3d7a9ba4c8ab96a64a119ff0d59aeba8e2f6f479
shinay <shinay@qq.com>
2013-12-10 21:01:47 +0800

    test comit

---
+++ Test/src/com/example/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
